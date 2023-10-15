12dfc55511461da150660e03f3be049a5b09b320
Vinicius Franca <vinicius.franca@meta.com.br>
2023-10-15 20:57:06 -0300

    teste deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { useEffect, useState } from 'react';
-/* import NavBar from '../components/NavBar' */
+import NavBar from '../components/NavBar'
 import AnimalList from '../components/AnimalList';
 
 function Home() {
   return (
     <div>
-      {/* <NavBar></NavBar> */}
+      <NavBar></NavBar>
       <main className="min-h-screen flex flex-col items-center justify-between  bg-gray-200">
         <div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
           <AnimalList />
